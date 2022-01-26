94efa3f76499a488dfe9f87064d0c17b5a6443eb
Jonathan Nguyen <jnganguyen3@gmail.com>
2022-01-25 21:11:32 -0500

    added a filler mentor page

--- pages/mentors.tsx
+++ pages/mentors.tsx
@@ -1,12 +1,30 @@
 // pages/mentors.tsx
 import type { NextPage } from 'next';
 import Layout from './components/layout';
+import Head from 'next/head';
 
 export default function Mentors() {
   return (
     <Layout>
       <div>
-        <a>This is the mentor page.</a>
+        <Head>
+          <title>Mentors</title>
+        </Head>
+        <h1 className="text-6xl">Meet the Mentors</h1>
+        <div className="flex">
+          <div>
+            <h3 className="text-3xl">James Walker</h3>
+          </div>
+          <div>
+            <h3 className="text-3xl">Jonathan Nguyen</h3>
+          </div>
+          <div>
+            <h3 className="text-3xl">Cameron Stuart</h3>
+          </div>
+          <div>
+            <h3 className="text-3xl">Adam Head</h3>
+          </div>
+        </div>
       </div>
     </Layout>
   )
